322effd39a5cc024a8812315baa484f21ed3bf74
Mike Nestor <mnestor@users.noreply.github.com>
2021-10-17 12:00:19 -0400

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -28,5 +28,5 @@ echo "Running backup: ${PGDATABASE}"
 pg_dump -h ${PGHOST} -Fc ${PGDATABASE} > /dump/dump_`date +%Y-%m-%d"_"%H_%M_%S`.psql
 
 echo "Deleteing extra backups and only keeping: ${NUM_KEEP}"
-ls -t /dump/dump*.psql | sort | sed -e "1,${NUM_KEEP}d" | xargs -r -d '\n' rm -- {}
+ls -1tr /dump/dump*.psql | head -n -${NUM_KEEP} | xargs -d '\n' rm -f --
 exit 0
